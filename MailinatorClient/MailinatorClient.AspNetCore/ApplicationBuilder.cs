9945c9401826ce91bd48dbb4c3bde7c8a9577d3e
undecidedapollo <undecidedapollo@gmail.com>
2017-05-17 19:03:32 -0400

    Added some code for the asp.net dependency injector.

---
+++ MailinatorClient/MailinatorClient.AspNetCore/ApplicationBuilder.cs
@@ -0,0 +1,19 @@
+﻿using MailinatorClient.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MailinatorClient.AspNetCore
+{
+    public static class MailinatorApplicationBuilder
+    {
+        public static IServiceCollection SetupMailinatorClient(this IServiceCollection app, string apiKey)
+        {
+            if (app == null) throw new ArgumentNullException($"{nameof(app)} cannot be null.");
+            if (apiKey == null) throw new ArgumentNullException($"{nameof(apiKey)} cannot be null.");
+
+            app.AddTransient(x => new Core.MailinatorClient(apiKey));
+
+            return app;
+        }
+    }
+}
